ef057492f6079c3ab53d5814235cb0b2ac9bcdc6
unknown <doomsdayprod1337@gmail.com>
2025-08-25 07:13:15 +0000

    fix: Fix Register.jsx api import and update Netlify build configuration

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate, useSearchParams } from 'react-router-dom';
-import axios from 'axios';
+import api from '../config/axios';
 
 const Register = () => {
   const navigate = useNavigate();
